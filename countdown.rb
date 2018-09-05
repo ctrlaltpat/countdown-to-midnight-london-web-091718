64ec977195ac9282613f66b9b5126d275aae7152
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-05 19:40:08 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,14 @@
 #write your code here
 
 def countdown(int)
-  counter = int
-  # while counter > 0
-  
+  number = int
+  while counter > 0
+    "#{number} SECOND(S)!"
+    counter -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  number = int
 end
